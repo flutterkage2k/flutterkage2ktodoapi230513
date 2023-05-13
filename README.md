f26ee4147286f451159a4635e12b8d8459fb11ba
flutterkage2k <jinheesung1@gmail.com>
2023-05-13 22:11:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# flutterkage2ktodoapi230513
+
+todo api crud
+
+## 練習
+
---
+++ android/app/src/main/kotlin/com/flutterkage2k/flutterkage2ktodoapi230513/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterkage2k.flutterkage2ktodoapi230513
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
